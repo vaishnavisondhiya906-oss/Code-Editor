fd7da8ec1e4f6244b797cd6c10da08e023f5c9d1
vaishnavisondhiya906-oss <vaishnavisondhiya906@gmail.com>
2026-05-10 00:06:49 +0530

    fixed login api

--- server/public/js/login.js
+++ server/public/js/login.js
@@ -14,7 +14,7 @@ async function login(e) {
   btn.disabled = true;
 
   try {
-    const res = await fetch("/api/login", {
+    const res = await fetch("https://online-code-editor-backend-vowg.onrender.com/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
